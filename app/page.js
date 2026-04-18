75da0bc98074564e7ad857e2c5749f12a320ffee
Suvarna <suvarna@Suvarnas-MacBook-Air.local>
2026-04-19 00:31:23 +0530

    fix homepage

--- app/page.js
+++ app/page.js
@@ -1,24 +1,8 @@
-"use client";
-
-import ThemeToggle from "../components/ThemeToggle";
-import SkillsChart from "../components/SkillsChart";
-import GitHubProjects from "../components/GitHubProjects";
-
 export default function Home() {
   return (
-    <main style={{ padding: "40px" }}>
-
+    <main>
       <h1>Suvarna Ramu</h1>
-      <p>Software Engineer | AI/ML Enthusiast</p>
-
-      <ThemeToggle />
-
-      <h2>Skills</h2>
-      <SkillsChart />
-
-      <h2>GitHub Projects</h2>
-      <GitHubProjects />
-
+      <p>Portfolio Website</p>
     </main>
   );
 }
